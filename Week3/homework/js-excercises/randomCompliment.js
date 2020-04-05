64c9d12a22607618f7b516329d0ee5315b97a4a1
Anis Alkomem <anis.alkumaim@gmail.com>
2020-04-05 11:18:11 +0200

    Modified the randomCompliment excercise

--- Week3/homework/js-excercises/randomCompliment.js
+++ Week3/homework/js-excercises/randomCompliment.js
@@ -1,11 +1,25 @@
-'use strict'
-function giveCompliment (name) {
-  const compliments = ['cool', 'great', 'funny', 'looking good', 'awesome', 'amazing', 'hilarious', 'something', 'one of a kind', 'the best'];
+"use strict";
+function giveCompliment(name) {
+  const compliments = [
+    "cool",
+    "great",
+    "funny",
+    "looking good",
+    "awesome",
+    "amazing",
+    "hilarious",
+    "something",
+    "one of a kind",
+    "the best",
+  ];
   // Getting a random number between two inclusive integer values according to the following formula Math.floor(Math.random() * (max-min +1)) +min; (source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random) , in our case min =0 , and max=9.
-  let i = Math.floor(Math.random()*10);
-  return `You are ${compliments[i]}, ${name}!`;
-}
-console.log(giveCompliment('Anis'));
-console.log(giveCompliment('Anis'));
-console.log(giveCompliment('Anis'));
+  // let i = Math.floor(Math.random()*10);  Instead of Choosing a random number hardcoded between 1-10, we can choose directly from the array
 
+  const randomCompliment =
+    compliments[Math.floor(Math.random() * compliments.length)];
+  // return `You are ${compliments[i]}, ${name}!`;
+  return `You are ${randomCompliment}, ${name}!`;
+}
+console.log(giveCompliment("Anis"));
+console.log(giveCompliment("Anis"));
+console.log(giveCompliment("Anis"));
